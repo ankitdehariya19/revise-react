9e64ecc3be6f2083afae743da86be7d79a8161be
ankitdehariya19 <131444868+ankitdehariya19@users.noreply.github.com>
2024-02-24 12:44:00 +0530

    episode01

---
+++ app.js
@@ -0,0 +1,5 @@
+const parent = React.createElement("div",{},React.createElement("h1",{},"its me hi form reactjs "));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
